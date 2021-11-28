a965aba1f5a37bda3a02f9f348c82fea9f0f64de
Andrew Stevenson <email>
2021-11-28 16:57:37 -0500

    working on form modal to submit a new gig

--- client/src/components/FormContent/FormContent.js
+++ client/src/components/FormContent/FormContent.js
@@ -1,9 +1,142 @@
 import "./FormContent.scss";
+import { Link } from "react-router-dom";
 
 function FormContent() {
   return (
     <div className="form">
-      <h1>Form Content</h1>
+      <h1>Post a Gig!</h1>
+      <form
+        className="form__container"
+        id="form__container"
+        name="myForm"
+        action="POST"
+        //   onSubmit={this.handleSubmit}
+      >
+        <div className="form__container-name">
+          <h4 className="form__container-name-title">Your Name</h4>
+          <input
+            className="form__container-name-input"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Please add your name"
+            // value={this.state.name}
+            // onChange={this.handleChange}
+          />
+          {/* <div
+                className="form__container-name-error"
+                id="nameError"
+                name="nameError"
+              >
+                <img
+                  src={error}
+                  alt="error"
+                  id="error-icon"
+                  style={{
+                    visibility: this.state.nameError ? "visible" : "hidden",
+                  }}
+                />
+                {this.state.nameError}
+              </div> */}
+        </div>
+        <div className="form__container-description">
+          <h4 className="form__container-description-title">Gig Description</h4>
+          <textarea
+            className="form__container-description-input"
+            type="text"
+            id="description"
+            name="description"
+            placeholder="Please describe the gig"
+            // value={this.state.name}
+            // onChange={this.handleChange}
+          />
+          {/* <div
+                className="form__container-description-error"
+                id="nameError"
+                name="nameError"
+              >
+                <img
+                  src={error}
+                  alt="error"
+                  id="error-icon"
+                  style={{
+                    visibility: this.state.nameError ? "visible" : "hidden",
+                  }}
+                />
+                {this.state.nameError}
+              </div> */}
+        </div>
+
+        <div className="form__container-category">
+          <h4 className="form__container-category-title">Category</h4>
+          <div className="form__container-category-list">
+            <div className="form__container-category-list-container">
+              <input
+                type="radio"
+                id="performance"
+                name="category"
+                value="performance"
+                // onChange={this.handleChange}
+              />
+              <label for="in-stock" id="in-label">
+                Performance
+              </label>
+            </div>
+            <div className="form__container-category-list-container2">
+              <input
+                type="radio"
+                id="recording"
+                name="category"
+                value="recording"
+                // onChange={this.handleChange}
+              />
+              <label for="out-of-stock" id="out-label">
+                Recording
+              </label>
+            </div>
+            <div className="form__container-category-list-container3">
+              <input
+                type="radio"
+                id="education"
+                name="category"
+                value="education"
+                // onChange={this.handleChange}
+              />
+              <label for="out-of-stock" id="out-label">
+                Education
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="form__container-venue">
+          <h4 className="form__container-venue-title">Venue</h4>
+          <input
+            className="form__container-venue-input"
+            type="text"
+            id="venue"
+            name="venue"
+            placeholder="Please add a venue"
+            // value={this.state.name}
+            // onChange={this.handleChange}
+          />
+          {/* <div
+                className="form__container-name-error"
+                id="nameError"
+                name="nameError"
+              >
+                <img
+                  src={error}
+                  alt="error"
+                  id="error-icon"
+                  style={{
+                    visibility: this.state.nameError ? "visible" : "hidden",
+                  }}
+                />
+                {this.state.nameError}
+              </div> */}
+        </div>
+      </form>
     </div>
   );
 }
