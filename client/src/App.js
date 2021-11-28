e417c60aed897255498ea13d3589ce3b00fd0702
Andrew Stevenson <email>
2021-11-28 16:25:50 -0500

    trying to implement OAuth login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 // import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import GigsPage from "./pages/GigsPage/GigsPage";
 import UsersPage from "./pages/UsersPage/UsersPage";
 import ContactUsPage from "./pages/ContactUsPage/ContactUsPage";
+import PrivateRoute from "./components/OAuth/PrivateRoute";
+import AuthButton from "./components/OAuth/AuthButton";
+import Login from "./components/OAuth/Login";
+import PublicPage from "./components/OAuth/PublicPage";
+import ProtectedPage from "./components/OAuth/ProtectedPage";
+
+export const API_URL = "http://localhost:8080";
 
 function App() {
   return (
     <div className="App">
       <Router onUpdate={() => window.scrollTo(0, 0)}>
         <Header />
+        <AuthButton />
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/users" exact component={UsersPage} />
@@ -22,6 +38,9 @@ function App() {
 
           {/* <Route path="/:id" component={HomePage} /> */}
         </Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/public" component={PublicPage} />
+        <PrivateRoute path="/protected" component={ProtectedPage} />
       </Router>
     </div>
   );
