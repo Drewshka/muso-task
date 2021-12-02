f1d75a099e973c579cee42dbd67e160c2646b2e8
Andrew Stevenson <email>
2021-12-02 18:45:04 -0500

    got a basic carousel working for categories of gigs.  I want to include some background photos of these categories in the carousel

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "./HomePage.scss";
 import React from "react";
+import Carousel from "../../components/Carousel/Carousel";
 // import Photo from "../../assets/images/Upload-video-bike.jpg";
 // import axios from "axios";
 // const apiUrl = "http://localhost:8080";
@@ -9,6 +10,20 @@ export default function HomePage() {
   return (
     <section className="home">
       <h1>Home Page</h1>
+      <Carousel>
+        <div>
+          <h1>Performance opportunities</h1>
+        </div>
+        <div>
+          <h1>Recording and studio work</h1>
+        </div>
+        <div>
+          <h1>Teaching opportunities</h1>
+        </div>
+        {/* <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" /> */}
+      </Carousel>
     </section>
   );
 }
