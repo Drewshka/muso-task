a23e40f2c86c97f00d788c36d01c3754d185fe44
Andrew Stevenson <email>
2021-12-05 19:59:33 -0500

    added styling for fonts at different breakpoints

--- client/src/pages/ContactUsPage/ContactUsPage.js
+++ client/src/pages/ContactUsPage/ContactUsPage.js
@@ -2,15 +2,10 @@ import { Link } from "react-router-dom";
 import "./ContactUsPage.scss";
 import React, { Component } from "react";
 
-// import Photo from "../../assets/images/Upload-video-bike.jpg";
-// import axios from "axios";
-// const apiUrl = "http://localhost:8080";
-
-// export default function GigsPage() {
 export default class ContactUsPage extends Component {
   render() {
     return (
-      <section className="users">
+      <section>
         <h1>Contact Us</h1>
       </section>
     );
